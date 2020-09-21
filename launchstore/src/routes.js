8c7ab9ebf16f14078c6ac49db1420c0c53de48f2
Michel Bernardo <bernardoenglish@outlook.com>
2020-09-21 07:37:54 -0400

    uploading project

---
+++ launchstore/src/routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const routes = express.Router();
+
+routes.get("/", (request, response) => {
+    return response.render("layout.njk");
+})
+
+module.exports = routes;
