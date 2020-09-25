d6255ea073239c128755482b7f495953867a105a
Michel Bernardo <bernardoenglish@outlook.com>
2020-09-24 20:41:51 -0400

    adding product form style

--- launchstore/src/routes.js
+++ launchstore/src/routes.js
@@ -1,8 +1,15 @@
-const express = require("express");
+const express = require('express');
 const routes = express.Router();
+const ProductController = require('./app/controllers/ProductController')
 
 routes.get("/", (request, response) => {
     return response.render("layout.njk");
+})
+
+routes.get("/products/create", ProductController.create);
+
+routes.get("/ads/create", (request, response) => {
+    return response.redirect("/products/create");
 })
 
 module.exports = routes;
