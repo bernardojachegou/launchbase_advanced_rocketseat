d5ba0ccb08b42f078bfdb62f35cc0c77923a2534
Michel Bernardo <bernardoenglish@outlook.com>
2020-10-09 22:01:04 -0400

    adding photos upload settings

--- launchstore/public/scripts.js
+++ launchstore/public/scripts.js
@@ -23,3 +23,38 @@ function handleDeleteConfirmation() {
         Event.preventDefault()
     }
 }
+
+// Função para gerênciamento de fotos
+const PhotosUpload = {
+    uploadLimit: 6,
+    handleFileInput(event) {
+        const { files: fileList } = event.target;
+        const { uploadLimit } = PhotosUpload;
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return
+        }
+
+        Array.from(fileList).forEach(file => {
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                const image = new Image();
+                image.src = String(reader.result);
+
+                const div = document.createElement('div');
+
+                div.classList.add('photo');
+                div.onclick = () => alert('remover foto');
+                div.appendChild(image);
+
+                document.querySelector('#photos-preview').appendChild(div);
+
+            }
+
+            reader.readAsDataURL(file)
+        })
+    }
+}
