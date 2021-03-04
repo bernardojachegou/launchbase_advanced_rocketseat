6c8e5f7690855ba99f62e9399fb763de0eb9c592
Michel Bernardo <bernardoenglish@outlook.com>
2021-03-03 20:39:44 -0400

    setting variable to database name

--- launchstore/src/config/db.js
+++ launchstore/src/config/db.js
@@ -1,9 +1,11 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
+
+const db_name = 'LaunchStore - Launch Base Modules';
 
 module.exports = new Pool({
-    user: 'postgres',
-    password: 'defcon4',
-    host: 'localhost',
-    port: '5432',
-    database: 'launchstore'
+  user: 'postgres',
+  password: 'defcon4',
+  host: 'localhost',
+  port: '5432',
+  database: db_name,
 });
