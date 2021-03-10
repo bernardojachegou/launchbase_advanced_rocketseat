7535afe6533ede54b98e121527aa8b1cae927f4d
Michel Bernardo <bernardoenglish@outlook.com>
2021-03-09 22:01:51 -0400

    adding register form into the launchstore

--- launchstore_login/src/routes/users.js
+++ launchstore_login/src/routes/users.js
@@ -15,7 +15,7 @@ const UserController = require('../app/controllers/UserController');
 // routes.post('/password-reset', SessionController.reset);
 
 // // user register UserController
-// routes.get('/register', UserController.registerForm);
+routes.get('/register', UserController.registerForm);
 // routes.post('/register', UserController.post);
 // routes.get('/', UserController.show);
 // routes.put('/', UserController.update);
