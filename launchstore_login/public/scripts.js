8215d11b8cc7f2754271493cf84a325b0556fb8c
Michel Bernardo <bernardoenglish@outlook.com>
2021-03-10 07:22:27 -0400

    adding regex into into cpf_cnpj, cep and mail mask

--- launchstore_login/public/scripts.js
+++ launchstore_login/public/scripts.js
@@ -1,173 +1,228 @@
-// Mask function used to format price;
+// Mask functions;
 const Mask = {
-    apply(input, func) {
-        input.value = input.value.replace(/\D/g, "")
-        setTimeout(function () {
-            input.value = Mask[func](input.value)
-        }, 1)
-    },
-    formatBRL(value) {
-        value = value.replace(/\D/g, "");
+  apply(input, func) {
+    input.value = input.value.replace(/\D/g, '');
+    setTimeout(function () {
+      input.value = Mask[func](input.value);
+    }, 1);
+  },
+  formatBRL(value) {
+    value = value.replace(/\D/g, '');
 
-        return new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-        }).format(value / 100)
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(value / 100);
+  },
+  cpfCnpj(value) {
+    value = value.replace(/\D/g, '');
+
+    if (value.length > 14) {
+      value = value.slice(0, -1);
     }
-}
+
+    if (value.length > 11) {
+      value = value.replace(/(\d{2})(\d)/, '$1.$2');
+      value = value.replace(/(\d{3})(\d)/, '$1.$2');
+      value = value.replace(/(\d{3})(\d)/, '$1/$2');
+      value = value.replace(/(\d{4})(\d)/, '$1-$2');
+    } else {
+      value = value.replace(/(\d{3})(\d)/, '$1.$2');
+      value = value.replace(/(\d{3})(\d)/, '$1.$2');
+      value = value.replace(/(\d{3})(\d)/, '$1-$2');
+    }
+    return value;
+  },
+  cep(value) {
+    value = value.replace(/\D/g, '');
+
+    if (value.length > 8) {
+      value = value.slice(0, -1);
+    }
+
+    value = value.replace(/(\d{5})(\d)/, '$1-$2');
+    return value;
+  },
+};
 
 // Confirmation function used to confirm delete option;
 function handleDeleteConfirmation() {
-    const confirmation = confirm("Confirmar?")
-    if (!confirmation) {
-        Event.preventDefault()
-    }
+  const confirmation = confirm('Confirmar?');
+  if (!confirmation) {
+    Event.preventDefault();
+  }
 }
 
 // Object created to handle photos update;
 const PhotosUpload = {
+  input: '',
+  preview: document.querySelector('#photos-preview'),
+  files: [],
+  uploadLimit: 6,
 
-    input: "",
-    preview: document.querySelector('#photos-preview'),
-    files: [],
-    uploadLimit: 6,
+  handleFileInput(event) {
+    const { files: fileList } = event.target;
+    PhotosUpload.input = event.target;
 
-    handleFileInput(event) {
-        const { files: fileList } = event.target;
-        PhotosUpload.input = event.target;
+    if (PhotosUpload.hasLimit(event)) return;
 
-        if (PhotosUpload.hasLimit(event)) return
+    Array.from(fileList).forEach((file) => {
+      PhotosUpload.files.push(file);
 
-        Array.from(fileList).forEach(file => {
+      const reader = new FileReader();
 
-            PhotosUpload.files.push(file);
+      reader.onload = () => {
+        const image = new Image();
+        image.src = String(reader.result);
 
-            const reader = new FileReader();
+        const div = PhotosUpload.getContainer(image);
 
-            reader.onload = () => {
-                const image = new Image();
-                image.src = String(reader.result);
+        PhotosUpload.preview.appendChild(div);
+      };
 
-                const div = PhotosUpload.getContainer(image);
+      reader.readAsDataURL(file);
+    });
 
-                PhotosUpload.preview.appendChild(div);
-            }
+    PhotosUpload.input.files = PhotosUpload.getAllFiles();
+  },
+  hasLimit(event) {
+    const { uploadLimit, input, preview } = PhotosUpload;
+    const { files: fileList } = input;
 
-            reader.readAsDataURL(file)
-        })
-
-        PhotosUpload.input.files = PhotosUpload.getAllFiles();
-    },
-    hasLimit(event) {
-        const { uploadLimit, input, preview } = PhotosUpload;
-        const { files: fileList } = input;
-
-        if (fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} fotos`)
-            event.preventDefault()
-            return true;
-        }
-
-        const photosDiv = []
-        preview.childNodes.forEach(item => {
-            if (item.classList && item.classList.value == 'photo') {
-                photosDiv.push(item)
-            }
-        })
-
-        const totalPhotos = fileList.length + photosDiv.length;
-        if (totalPhotos > uploadLimit) {
-            alert('Você atingou o limite máximo de fotos')
-            event.preventDefault()
-            return true;
-        }
-
-        return false;
-    },
-    getAllFiles() {
-        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer();
-
-        PhotosUpload.files.forEach(file => dataTransfer.items.add(file));
-
-        return dataTransfer.files;
-    },
-    getContainer(image) {
-        const div = document.createElement('div');
-
-        div.classList.add('photo');
-
-        div.onclick = PhotosUpload.removePhoto;
-
-        div.appendChild(image);
-
-        div.appendChild(PhotosUpload.getRemoveButton());
-
-        return div;
-    },
-    getRemoveButton() {
-        const button = document.createElement('i');
-        button.classList.add('material-icons');
-        button.innerHTML = "close";
-        return button;
-    },
-    removePhoto(event) {
-        const photoDiv = event.target.parentNode; //<div class="photo">
-        const photosArray = Array.from(PhotosUpload.preview.children);
-        const index = photosArray.indexOf(photoDiv);
-
-        PhotosUpload.files.splice(index, 1)
-        PhotosUpload.input.files = PhotosUpload.getAllFiles()
-
-        photoDiv.remove();
-    },
-    removeOldPhoto(event) {
-        const photoDiv = event.target.parentNode;
-
-        if (photoDiv.id) {
-            const removedFiles = document.querySelector('input[name="removed_files"]');
-            if (removedFiles) {
-                removedFiles.value += `${photoDiv.id},`
-            }
-        }
-
-        photoDiv.remove();
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`);
+      event.preventDefault();
+      return true;
     }
-}
+
+    const photosDiv = [];
+    preview.childNodes.forEach((item) => {
+      if (item.classList && item.classList.value == 'photo') {
+        photosDiv.push(item);
+      }
+    });
+
+    const totalPhotos = fileList.length + photosDiv.length;
+    if (totalPhotos > uploadLimit) {
+      alert('Você atingou o limite máximo de fotos');
+      event.preventDefault();
+      return true;
+    }
+
+    return false;
+  },
+  getAllFiles() {
+    const dataTransfer =
+      new ClipboardEvent('').clipboardData || new DataTransfer();
+
+    PhotosUpload.files.forEach((file) => dataTransfer.items.add(file));
+
+    return dataTransfer.files;
+  },
+  getContainer(image) {
+    const div = document.createElement('div');
+
+    div.classList.add('photo');
+
+    div.onclick = PhotosUpload.removePhoto;
+
+    div.appendChild(image);
+
+    div.appendChild(PhotosUpload.getRemoveButton());
+
+    return div;
+  },
+  getRemoveButton() {
+    const button = document.createElement('i');
+    button.classList.add('material-icons');
+    button.innerHTML = 'close';
+    return button;
+  },
+  removePhoto(event) {
+    const photoDiv = event.target.parentNode; //<div class="photo">
+    const photosArray = Array.from(PhotosUpload.preview.children);
+    const index = photosArray.indexOf(photoDiv);
+
+    PhotosUpload.files.splice(index, 1);
+    PhotosUpload.input.files = PhotosUpload.getAllFiles();
+
+    photoDiv.remove();
+  },
+  removeOldPhoto(event) {
+    const photoDiv = event.target.parentNode;
+
+    if (photoDiv.id) {
+      const removedFiles = document.querySelector(
+        'input[name="removed_files"]'
+      );
+      if (removedFiles) {
+        removedFiles.value += `${photoDiv.id},`;
+      }
+    }
+
+    photoDiv.remove();
+  },
+};
 
 // Object created to handle image selection inside show page;
 const ImageGallery = {
-    highlight: document.querySelector('.gallery .highlight > img'),
-    previews: document.querySelectorAll('.gallery-preview img'),
+  highlight: document.querySelector('.gallery .highlight > img'),
+  previews: document.querySelectorAll('.gallery-preview img'),
 
-    setImage(event) {
-        const { target } = event;
+  setImage(event) {
+    const { target } = event;
 
-        ImageGallery.previews.forEach(preview => preview.classList.remove('active'));
+    ImageGallery.previews.forEach((preview) =>
+      preview.classList.remove('active')
+    );
 
-        target.classList.add('active');
+    target.classList.add('active');
 
-        ImageGallery.highlight.src = target.src;
-        LightBox.image.src = target.src;
-    }
-}
+    ImageGallery.highlight.src = target.src;
+    LightBox.image.src = target.src;
+  },
+};
 
 // Object created to handle the image modal;
 const LightBox = {
-    target: document.querySelector('.lightbox-target'),
-    image: document.querySelector('.lightbox-target img'),
-    closeButton: document.querySelector('.lightbox-target a.lightbox-close'),
+  target: document.querySelector('.lightbox-target'),
+  image: document.querySelector('.lightbox-target img'),
+  closeButton: document.querySelector('.lightbox-target a.lightbox-close'),
 
-    open() {
-        LightBox.target.style.opacity = 1;
-        LightBox.target.style.top = 0;
-        LightBox.target.style.bottom = 0;
-        LightBox.closeButton.style.top = 0;
-    },
-    close() {
-        LightBox.target.style.opacity = 0;
-        LightBox.target.style.top = "-100%";
-        LightBox.target.style.bottom = 'initial';
-        LightBox.closeButton.style.top = "-80px";
+  open() {
+    LightBox.target.style.opacity = 1;
+    LightBox.target.style.top = 0;
+    LightBox.target.style.bottom = 0;
+    LightBox.closeButton.style.top = 0;
+  },
+  close() {
+    LightBox.target.style.opacity = 0;
+    LightBox.target.style.top = '-100%';
+    LightBox.target.style.bottom = 'initial';
+    LightBox.closeButton.style.top = '-80px';
+  },
+};
 
-    },
-}
+const Validate = {
+  apply(input, func) {
+    let results = Validate[func](input.value);
+    input.value = results.value;
+
+    if (results.error) {
+      alert(results.error);
+      input.focus();
+    }
+  },
+  isEmail(value) {
+    let error = null;
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!value.match(mailFormat)) {
+      error = 'Invalid Email';
+    }
+    return {
+      error,
+      value,
+    };
+  },
+};
