41b10ddbee45bf29c2f872fa41b95a4754245731
Michel Bernardo <bernardoenglish@outlook.com>
2021-03-21 20:46:18 -0400

    trying to get login working correclty but I got some problem, solve it tomorrow

--- launchstore_login/src/routes/users.js
+++ launchstore_login/src/routes/users.js
@@ -2,10 +2,11 @@ const express = require('express');
 const routes = express.Router();
 const SessionController = require('../app/controllers/SessionController');
 const UserController = require('../app/controllers/UserController');
-const Validator = require('../app/validators/user');
+const UserValidator = require('../app/validators/user');
+const SessionValidator = require('../app/validators/session');
 
 routes.get('/login', SessionController.loginForm);
-// routes.post('/login', SessionController.login);
+routes.post('/login', SessionValidator.login, SessionController.login);
 routes.post('/logout', SessionController.logout);
 
 // routes.get('/forgot-password', SessionController.forgotForm);
@@ -14,9 +15,9 @@ routes.post('/logout', SessionController.logout);
 // routes.post('/password-reset', SessionController.reset);
 
 routes.get('/register', UserController.registerForm);
-routes.post('/register', Validator.post, UserController.post);
-routes.get('/', Validator.show, UserController.show);
-routes.put('/', Validator.update, UserController.update);
+routes.post('/register', UserValidator.post, UserController.post);
+routes.get('/', UserValidator.show, UserController.show);
+routes.put('/', UserValidator.update, UserController.update);
 // routes.delete('/', UserController.delete);
 
 module.exports = routes;
